8a3dcc35154de5ad180b504f7bd061979c14d4c4
Michael Martino <Mick666@hotmail.com.au>
2020-07-13 04:17:22 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+const Blog = require('./models/blogs')
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response, next) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
+})
